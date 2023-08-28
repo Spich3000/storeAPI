e20b42e57e0ded3bd12a17138af7ae385396eb4f
Spich <spichakovda@gmail.com>
2023-08-28 04:15:34 +0300

    set up the project

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log("zdorovo");
+
+require("dotenv").config()
+
+// async errors
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get("/", (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products routes</a>')
+})
+
+//products route
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 5050
+
+const start = async () => {
+    try {
+        // connect db
+        app.listen(port, console.log(`Server is listening port ${port}`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
