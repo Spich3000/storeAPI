9d8bfc0efd33910e6955f8fa7f12a47fb1a79e75
Spich <spichakovda@gmail.com>
2023-08-29 03:35:38 +0300

    connect db, create products route

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ require("dotenv").config()
 const express = require("express");
 const app = express();
 
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products")
+
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 
@@ -17,6 +20,8 @@ app.get("/", (req, res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">products routes</a>')
 })
 
+app.use("/api/v1/products", productsRouter)
+
 //products route
 app.use(notFoundMiddleware)
 app.use(errorMiddleware)
@@ -26,6 +31,7 @@ const port = process.env.PORT || 5050
 const start = async () => {
     try {
         // connect db
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening port ${port}`));
     } catch (error) {
         console.log(error);
