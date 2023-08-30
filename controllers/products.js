679e5777d9d6bf5056567d68667b3f716a8814ec
Spich <spichakovda@gmail.com>
2023-08-30 22:55:38 +0300

    custom request param

--- controllers/products.js
+++ controllers/products.js
@@ -4,14 +4,23 @@ const product = require("../models/product")
 const Product = require("../models/product")
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({ 
-        featured: true, 
+    const products = await Product.find({
+        page: "2",
     })
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: "products route" })
+    const { featured } = req.query
+
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === "true" ? true : false
+    }
+
+    const products = await Product.find(queryObject)
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
