e00dc31260fa434755b29f0a68439e5e61f4c125
Spich <spichakovda@gmail.com>
2023-08-31 05:18:10 +0300

    sort

--- controllers/products.js
+++ controllers/products.js
@@ -1,31 +1,41 @@
 
 
-const product = require("../models/product")
 const Product = require("../models/product")
 
 const getAllProductsStatic = async (req, res) => {
-    const search = "ab"
-    const products = await Product.find({
-        name: { $regex: search, $options: "i" },
-    })
+    const products = await Product.find({}).sort("name")
     res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query
+    const { featured, company, name, sort } = req.query
     const queryObject = {}
 
     if (featured) {
         queryObject.featured = featured === "true" ? true : false
     }
+
     if (company) {
         queryObject.company = company
     }
+
     if (name) {
         queryObject.name = { $regex: name, $options: "i" }
     }
 
-    const products = await Product.find(queryObject)
+    let result = Product.find(queryObject)
+
+    if (sort) {
+        const sortList = sort.split(",").join(" ");
+        result = result.sort(sortList)
+        // console.log(sort);
+        // products = products.sort()
+    } else {
+        result = result.sort("createdAt")
+    }
+
+    const products = await result
+
     res.status(200).json({ products, nbHits: products.length })
 }
 
