1ffd7170af2c42ec2848e7b3dfe541d6e2a89403
Spich <spichakovda@gmail.com>
2023-08-31 19:48:10 +0300

    page, limit, skip

--- controllers/products.js
+++ controllers/products.js
@@ -3,7 +3,11 @@
 const Product = require("../models/product")
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({}).select("name price").limit(4)
+    const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .limit(4)
+    //.skip(1) // skip firdst item
     res.status(200).json({ products, nbHits: products.length })
 }
 
@@ -37,6 +41,12 @@ const getAllProducts = async (req, res) => {
         const fieldsList = fields.split(",").join(" ");
         result = result.select(fieldsList)
     }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page -1) * limit;
+
+    result = result.skip(skip).limit(limit).page(page);
 
     const products = await result
 
